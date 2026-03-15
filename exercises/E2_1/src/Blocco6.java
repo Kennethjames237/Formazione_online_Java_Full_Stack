26a242dca196c340419b80532334c3dbc1527796
nna minkousse kenneth james <kennethjames.nnaminkousse@studenti.unipr.it>
2026-03-15 13:44:03 +0100

    new structure

---
+++ exercises/E2_1/src/Blocco6.java
@@ -0,0 +1,6 @@
+public class Blocco6 {
+    public static void main(String[] args){
+        System.out.println("--------------------------26) Creare matrice 3x3 con Scanner--------------------------------");
+
+    }
+}
